a48d6a9aee6d47a07a52e123fef8e98ad79ea86b
EARNEST BLESSING <113962528+earnest2653@users.noreply.github.com>
2023-08-12 06:08:28 +0530

    Create 37.Write a program to generate the list of all factor for n value.c

---
+++ 37.Write a program to generate the list of all factor for n value.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void generateFactors(int n) {
+    printf("Factors of %d: ", n);
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    if (n <= 0) {
+        printf("Invalid input. Please enter a positive integer.\n");
+        return 1;
+    }
+
+    generateFactors(n);
+
+    return 0;
+}
