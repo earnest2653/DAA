969b6bd709c462e1d77a59f1dd5d4e6e43042cd0
EARNEST BLESSING <113962528+earnest2653@users.noreply.github.com>
2023-08-12 06:01:06 +0530

    Create 30.Write a program to find the sum of digits.c

---
+++ 30.Write a program to find the sum of digits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sumOfDigits(int num) {
+    int sum = 0;
+    while (num > 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    int result = sumOfDigits(num);
+
+    printf("Sum of digits: %d\n", result);
+
+    return 0;
+}
