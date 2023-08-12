3a4cacafd5a0dc42527e1e07f60eff5f367c540a
EARNEST BLESSING <113962528+earnest2653@users.noreply.github.com>
2023-08-12 05:30:11 +0530

    Create 15.Write a program to perform Strassen’s Matrix Multiplication.c

---
+++ 15.Write a program to perform Strassen’s Matrix Multiplication.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+
+void addMatrices(int n, int A[][n], int B[][n], int C[][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+void subtractMatrices(int n, int A[][n], int B[][n], int C[][n]) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            C[i][j] = A[i][j] - B[i][j];
+        }
+    }
+}
+
+void strassenMatrixMultiply(int n, int A[][n], int B[][n], int C[][n]) {
+    if (n == 1) {
+        C[0][0] = A[0][0] * B[0][0];
+        return;
+    }
+
+    int newSize = n / 2;
+
+    int A11[newSize][newSize], A12[newSize][newSize], A21[newSize][newSize], A22[newSize][newSize];
+    int B11[newSize][newSize], B12[newSize][newSize], B21[newSize][newSize], B22[newSize][newSize];
+    int C11[newSize][newSize], C12[newSize][newSize], C21[newSize][newSize], C22[newSize][newSize];
+
+    // Divide matrices A and B into submatrices
+    // ...
+
+    // Calculate the seven products using recursive calls
+    // ...
+
+    // Combine the submatrices to form matrix C
+    // ...
+
+}
+
+int main() {
+    int n;
+
+    printf("Enter the size of matrices (power of 2): ");
+    scanf("%d", &n);
+
+    int A[n][n], B[n][n], C[n][n];
+
+    printf("Enter elements of matrix A:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    printf("Enter elements of matrix B:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    strassenMatrixMultiply(n, A, B, C);
+
+    printf("Resultant matrix C after Strassen's multiplication:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", C[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
